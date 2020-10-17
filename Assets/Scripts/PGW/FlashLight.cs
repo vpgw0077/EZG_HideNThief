bd69fb1959db576ed86023e7884ffe2d6e86ddc8
GunWoo Park <vpgw0077@naver.com>
2020-10-18 00:13:56 +0900

    20.10.18
    손전등 on/off

---
+++ Assets/Scripts/PGW/FlashLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public Light Light;
+    public bool isON = true;
+
+
+    void Update()
+    {
+        TryOn();
+        if (isON)
+        {
+            print("ggg");
+        }
+    }
+
+    public void TryOn()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            LightON();
+        }
+    }
+    public void LightON()
+    {
+        isON = !isON;
+        if (isON)
+        {
+            Light.enabled = true;
+        }
+
+        else
+        {
+            Light.enabled = false;
+        }
+    }
+}
