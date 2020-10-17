8ddacb35e2151a88de2e2007e348bb65d9f7c15a
jongsun <jongsun_2011@naver.com>
2020-10-17 21:13:30 +0900

    연동 및 달리기&스태미너 추가
    연동, 달리기, 스태미너 추가

--- Assets/Scripts/LSH/Characters/FirstPersonMovement.cs
+++ Assets/Scripts/LSH/Characters/FirstPersonMovement.cs
@@ -1,19 +1,62 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    public float speed = 5;
-    Vector2 velocity;
+    float recoverTime = 0;
 
+    public float Movespeed = 5f;
+    public Slider Stamina_Bar;
+    Vector2 velocity;
 
     void Update()
     {
+        Run();
     }
     private void FixedUpdate()
     {
-        velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        velocity.y = Input.GetAxis("Vertical") * Movespeed * Time.deltaTime;
+        velocity.x = Input.GetAxis("Horizontal") * Movespeed * Time.deltaTime;
         transform.Translate(velocity.x, 0, velocity.y);
+    }
+
+    void Run()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                if (Stamina_Bar.value > 0)
+                {
+                    Movespeed = 15f;
+                    Stamina_Bar.value -= 0.004f;
+                    recoverTime = 0;
+                }
+                else if (Stamina_Bar.value == 0)
+                {
+                    Movespeed = 3f;
+                    recoverTime = 0;
+                }
+            }
+        }
+        else 
+        {
+            Movespeed = 5f;
+
+            if (Stamina_Bar.value > 0)
+            {
+                recoverTime += Time.deltaTime;
+            }
+            else if (Stamina_Bar.value == 0)
+            {
+                Movespeed = 2f;
+                recoverTime += Time.deltaTime;
+            }
+        }
+        if (recoverTime > 1)
+        {
+            Stamina_Bar.value += Time.deltaTime;
+        }
     }
 
 }
